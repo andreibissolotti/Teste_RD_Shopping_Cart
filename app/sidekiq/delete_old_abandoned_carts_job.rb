22bf27e5e18004a000bf10edd923c3118fb5e7b4
Andrei Bissolotti <andreibisso@gmail.com>
2026-01-30 22:53:58 -0300

    Configura container para o sidekiq

--- app/sidekiq/delete_old_abandoned_carts_job.rb
+++ app/sidekiq/delete_old_abandoned_carts_job.rb
@@ -2,6 +2,8 @@ class DeleteOldAbandonedCartsJob
   include Sidekiq::Job
 
   def perform
+    Rails.logger.info 'Deleting old abandoned carts'
     Carts::DeleteOldAbandonedService.call
+    Rails.logger.info 'Old abandoned carts deleted'
   end
 end
--- app/sidekiq/mark_cart_as_abandoned_job.rb
+++ app/sidekiq/mark_cart_as_abandoned_job.rb
@@ -2,6 +2,8 @@ class MarkCartAsAbandonedJob
   include Sidekiq::Job
 
   def perform
+    Rails.logger.info 'Marking cart as abandoned'
     Carts::MarkAbandonedService.call
+    Rails.logger.info 'Cart marked as abandoned'
   end
 end
